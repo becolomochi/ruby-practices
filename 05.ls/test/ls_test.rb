8af9cfbcfa34e1d9cf1dd56dc018aeae87eb9fc9
becolomochi <becolomochi@gmail.com>
2020-12-03 11:21:49 +0900

    rubocopエラーを分かる範囲で対応

--- 05.ls/test/ls_test.rb
+++ 05.ls/test/ls_test.rb
@@ -1,22 +1,15 @@
+# frozen_string_literal: true
 require 'minitest/autorun'
 require './ls'
 
 class LsTest < Minitest::Test
-  def test_convert_filetype
+  def test_type
     assert_equal '-', convert_filetype('file')
     assert_equal 'd', convert_filetype('directory')
-    # assert_equal '-', convert_filetype('unknown')
   end
 
-  def test_convert_permission
-    # assert_equal '-rwxr--r--', convert_permission(33252)
-    # assert_equal 'drwxr-xr-x', convert_permission(16877)
-    assert_equal "rwxr--r--", convert_permission(33252)
-    assert_equal "rwxr-xr-x", convert_permission(16877)
-    # assert_equal "rwxr--rx-", convert_permission(33252)
+  def test_permission
+    assert_equal 'rwxr--r--', permission(33252)
+    assert_equal 'rwxr-xr-x', permission(16877)
   end
-
-  # def test_convert_user
-  #   assert_equal "beco", convert_user(501)
-  # end
 end
