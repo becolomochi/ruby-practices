f5624d1ec7fd1d5fbfdc9891a582e7986363639d
becolomochi <becolomochi@gmail.com>
2020-12-17 14:51:57 +0900

    テストを動くようにした

--- 05.ls/test/ls_test.rb
+++ 05.ls/test/ls_test.rb
@@ -4,13 +4,24 @@ require 'minitest/autorun'
 require './ls'
 
 class LsTest < Minitest::Test
-  # def test_type
-  #   assert_equal '-', FileData#type_short('file')
-  #   assert_equal 'd', FileData#type_short('directory')
-  # end
+  def test_file_ls
+    test_file = FileData.new(File.expand_path('../ls.rb', __dir__))
+    assert_equal '-', test_file.type_short
+    assert_equal 'rwxr-xr-x', test_file.permission
+    assert_equal 1, test_file.nlink
+    assert_equal 'beco', test_file.user_name
+    assert_equal 'staff', test_file.group_name
+  end
 
-  # def test_permission
-  #   assert_equal 'rwxr--r--', FileData#permission(33_252)
-  #   assert_equal 'rwxr-xr-x', FileData#permission(16_877)
-  # end
+  def test_file_readme
+    test_file = FileData.new(File.expand_path('../../README.md', __dir__))
+    assert_equal '-', test_file.type_short
+    assert_equal 'rw-r--r--', test_file.permission
+  end
+
+  def test_file_directory
+    test_file = FileData.new(File.expand_path('../', __dir__))
+    assert_equal 'd', test_file.type_short
+    assert_equal 'rwxr-xr-x', test_file.permission
+  end
 end
