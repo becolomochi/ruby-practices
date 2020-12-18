a5c7950960b0b1d3455ae287bd6049bd5449f18e
becolomochi <becolomochi@gmail.com>
2020-12-18 15:42:45 +0900

    標準入力ありの出力まで

--- 06.wc/test/wc_test.rb
+++ 06.wc/test/wc_test.rb
@@ -5,16 +5,37 @@ require './wc'
 
 class WcTest < Minitest::Test
   def test_en_txt
-    file = Wc.new(File.expand_path('../sample_en.txt', __dir__))
+    file = WcFile.new(File.expand_path('../sample_en.txt', __dir__))
     assert_equal 2, file.count_line
     assert_equal 8, file.count_word
     assert_equal 34, file.count_byte
   end
 
   def test_ja_txt
-    file = Wc.new(File.expand_path('../sample_ja.txt', __dir__))
+    file = WcFile.new(File.expand_path('../sample_ja.txt', __dir__))
     assert_equal 3, file.count_line
     assert_equal 8, file.count_word
     assert_equal 144, file.count_byte
+  end
+
+  def text_stdin_current_directory
+    file = WcStdin.new(File.expand_path('.', __dir__))
+    assert_equal 5, file.count_line
+    assert_equal 5, file.count_word
+    assert_equal 48, file.count_byte
+  end
+
+  def text_stdin_parent_directory
+    file = WcStdin.new(File.expand_path('../', __dir__))
+    assert_equal 10, file.count_line
+    assert_equal 10, file.count_word
+    assert_equal 109, file.count_byte
+  end
+
+  def text_stdin_grandparents_directory
+    file = WcStdin.new(File.expand_path('../../', __dir__))
+    assert_equal 5, file.count_line
+    assert_equal 5, file.count_word
+    assert_equal 74, file.count_byte
   end
 end
