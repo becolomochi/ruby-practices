54495d7b2f4a4ea4bf5e181a67e724b1ef59cbad
becolomochi <becolomochi@gmail.com>
2020-12-18 15:42:45 +0900

    標準入力ありの出力まで

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -7,12 +7,42 @@ require 'optparse'
 
 def main
   hash = option_parser
-  file = Wc.new(hash[:target])
+  if hash[:target]
+    file = WcFile.new(hash[:target])
+  else
+    line = ''
+    while string = STDIN.gets
+      break if string.chomp == "exit"
+      line += string
+    end
+    file = WcStdin.new(line)
+  end
   rows = create_rows(file, hash)
   puts rows.map {|row| row.rjust(8)}.join
 end
 
-class Wc
+class WcStdin
+  attr_reader :file_read, :name
+
+  def initialize(line)
+    @file_read = line
+    @name = ''
+  end
+  
+  def count_line
+    file_read.count("\n")
+  end
+  
+  def count_word
+    file_read.split(' ').length
+  end
+  
+  def count_byte
+    file_read.bytesize
+  end
+end
+
+class WcFile
   attr_reader :file_read, :name
 
   def initialize(file)
