0c2cb1ee4b94c1de5b2493e4956bfd4cbfc36a17
becolomochi <becolomochi@gmail.com>
2020-12-29 16:19:07 +0900

    サンプルファイルをsampleディレクトリに移動し、テストできるように修正

--- 06.wc/test/wc_test.rb
+++ 06.wc/test/wc_test.rb
@@ -5,37 +5,25 @@ require './wc'
 
 class WcTest < Minitest::Test
   def test_en_txt
-    file = WcFile.new(File.expand_path('../sample_en.txt', __dir__))
+    file = Wc.new(File.open(File.expand_path('../sample/sample_en.txt', __dir__)).read, 'sample/sample_en.txt')
     assert_equal 2, file.count_line
     assert_equal 8, file.count_word
     assert_equal 34, file.count_byte
   end
 
   def test_ja_txt
-    file = WcFile.new(File.expand_path('../sample_ja.txt', __dir__))
+    file = Wc.new(File.open(File.expand_path('../sample/sample_ja.txt', __dir__)).read, 'sample/sample_ja.txt')
     assert_equal 3, file.count_line
     assert_equal 8, file.count_word
     assert_equal 144, file.count_byte
   end
 
-  def text_stdin_current_directory
-    file = WcStdin.new(File.expand_path('.', __dir__))
-    assert_equal 5, file.count_line
-    assert_equal 5, file.count_word
-    assert_equal 48, file.count_byte
-  end
+  def test_stdin_sample_directory
+    output = `ls -l #{File.expand_path('../sample/', __dir__)}`
 
-  def text_stdin_parent_directory
-    file = WcStdin.new(File.expand_path('../', __dir__))
-    assert_equal 10, file.count_line
-    assert_equal 10, file.count_word
-    assert_equal 109, file.count_byte
-  end
-
-  def text_stdin_grandparents_directory
-    file = WcStdin.new(File.expand_path('../../', __dir__))
-    assert_equal 5, file.count_line
-    assert_equal 5, file.count_word
-    assert_equal 74, file.count_byte
+    file = Wc.new(output, '')
+    assert_equal 3, file.count_line
+    assert_equal 20, file.count_word
+    assert_equal 125, file.count_byte
   end
 end
